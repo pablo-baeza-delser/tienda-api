0c95f9eb289220bed1b0393dcdf677d146de674e
Pablo Baeza del Ser <pablobaezadelser@gmail.com>
2026-06-02 10:17:10 +0200

    feat: initial project setup

---
+++ src/main/java/com/tienda/api/TiendaApiApplication.java
@@ -0,0 +1,13 @@
+package com.tienda.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaApiApplication.class, args);
+	}
+
+}
